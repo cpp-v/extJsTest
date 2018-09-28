e52af8b09d0a80ae6a08bf70c2c1b24275bb516b
cpp-v <cpp_v@mail.ru>
2018-09-28 18:26:17 +0300

    add CppvPropGrid

---
+++ testCppvPropGrid/js/js.js
@@ -0,0 +1,94 @@
+//alert("1");
+Ext.onReady(function(){
+
+   var source= {
+            '(name)': 'cppvProperties Grid',
+            grouping: false,
+            autoFitColumns: true,
+            productionQuality: false,
+            created: new Date(Date.parse('10/15/2006')),
+            startTime: '10:00 AM',
+            tested: false,
+            version: 0.01,            
+            borderWidth: 1,
+            cb:'wwe',
+            testBool:true
+   };
+
+var combo = new Ext.form.ComboBox({
+    typeAhead: true,
+    triggerAction: 'all',
+    lazyRender:true,
+    mode: 'local',
+    store: new Ext.data.ArrayStore({
+        id: 0,
+        fields: [
+            'myId',
+            'displayText'
+        ],
+        data: [[1, 'item1'], [2, 'item2']]
+    }),
+    valueField: 'myId',
+    displayField: 'displayText'
+});
+
+
+   var custEditors={
+        'startTime': new Ext.grid.GridEditor(new Ext.form.TimeField({selectOnFocus:true})),
+        'cb': new Ext.grid.GridEditor(combo),
+        'testBool': new Ext.grid.GridEditor(new Ext.form.Checkbox({}))
+        
+    };
+    
+   var custRenderers={ 
+     testBool:function (v) {   console.log('custRenderers testBool');
+     	 if (v==true) return 'Да';
+     	 else return 'Нет';
+                           },
+                           
+     cb:function (v) {  console.log('custRenderers cb');
+     	 if (v!=null) return 'item'+v;     	 
+                           }                           
+  };       
+    
+   var prNames= {
+            tested: 'QA',
+            borderWidth: 'Border Width'
+        };    
+   var viewConf={
+            forceFit: true,
+            scrollOffset: 2 // the grid will never have scrollbars
+        };     
+
+   propsGrid = new Ext.grid.CppvPropertyGrid({
+        renderTo: 'prop-cppvgrid',
+        width: 300,
+        autoHeight: true,
+        propertyNames:prNames,
+        source: source,
+        customEditors:custEditors,
+        customRenderers:custRenderers,
+        viewConfig :viewConf 
+		});
+
+
+        
+// simulate updating the grid data via a button click
+var btn=new Ext.Button({
+        renderTo: 'button-container',
+        text: 'Update source',
+        handler: function(){
+            propsGrid.setSource({
+                '(name)': 'Property Grid',
+                grouping: false,
+                autoFitColumns: true,
+                productionQuality: true,
+                created: new Date(),
+                tested: false,
+                version: 0.8,
+                borderWidth: 2
+            });
+        }
+});        
+
+});
